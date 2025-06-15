8178a2fc4622e895032cefbd32d771ea6b36781e
Gert Steyn <gert@senseware.com>
2025-06-15 21:33:44 +0200

    Fix ignore paths

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,21 +3,27 @@
 set -e
 
 # Resolve UNISONLOCALHOSTNAME using cascading fallback
-export UNISONLOCALHOSTNAME="${UNISON_LOCAL_HOSTNAME:-${UNISONLOCALHOSTNAME:-${hostname:-}}}"
+export UNISONLOCALHOSTNAME="${LOCAL_HOSTNAME:-${UNISONLOCALHOSTNAME:-${hostname:-}}}"
 
 # Set UNISON archive folder path
 export UNISON="/data/.unison/${UNISONLOCALHOSTNAME}"
-UNISON_EXTRA="-terse"
-
 # Ensure archive folder exists
 mkdir -p "$UNISON"
 
+# remote
+if [ -n "$REMOTE_HOSTNAME" ]; then
+    REMOTE_DATA="socket://$REMOTE_HOSTNAME:5000//data"
+fi
+
 if [ -n "$REMOTE_DATA" ]; then
     # client
-    echo "Starting Unison Client: unison /data $REMOTE_DATA -auto -batch -repeat watch -ignore 'BelowPath .unison' -ignore 'Regex \.unison.*' $UNISON_EXTRA"
-    exec unison /data "$REMOTE_DATA" -auto -batch -repeat watch -ignore "BelowPath .unison" -ignore "Regex \.unison.*" $UNISON_EXTRA
+    echo "Starting Unison Client"
+    exec unison /data "$REMOTE_DATA" -auto -batch -repeat watch -terse \
+      -ignore "BelowPath .unison" \
+      -ignore "Regex \.unison.*" \
+      -ignore "Name *.swp"
 else
     # server
-    echo "Starting Unison Server: unison -socket 5000 -auto -batch -repeat watch $UNISON_EXTRA"
-    exec unison -socket 5000 -auto -batch -repeat watch $UNISON_EXTRA
+    echo "Starting Unison Server"
+    exec unison -socket 5000 -auto -batch -repeat watch -terse
 fi
